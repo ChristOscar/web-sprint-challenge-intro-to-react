d8d0aaa5b6b378cf16f6fe63fa37d9eeb9abef02
Christwide Oscar <triple101.games@gmail.com>
2021-12-10 14:12:30 -0600

    Got it working ging to add button soon. Only diplaying 6 people

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,23 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people')
+  .then(resp => {
+    setCharacters(resp.data);
+  })
+  .catch(() => console.error('Oops! Something went wrong...wanna try again?'))
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Characters In Star Wars</h1>
+      {characters.map(char => {
+        return <Character key={char.name} data={char} />
+      })
+    }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,56 @@
 // Write your Character component here
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+
+const kf = keyframes`
+  50% {
+    transform: scale(0.8);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`
+
+const StyledDetails = styled.div`
+    color: #ffe81f;
+    width: 60%;
+    height: 60%;
+    text-align: center;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-center;
+    margin-left: 18%;
+    margin-bottom: 5%;
+    padding: 2%;
+    background-color: #B2D8D8;
+    border-radius: 10%;
+    border: 5px solid #fff;
+    transform: scale(2);
+    opacity: 0; 
+    animation: ${kf} 0.3s ease-in-out forwards;
+    h1 {
+        font-size: 2.5rem;
+    }
+    h3 {
+        font-size: 1.5rem;
+    }
+`
+
+
+export default function Character(props) {
+    const { data } =props;
+
+    return (
+            <StyledDetails>
+                <h1>{data.name}:</h1>
+                <h3>Born in {data.birth_year}</h3>
+                <h3>Gender: {data.gender}</h3>
+                <h3>Height: {data.height}</h3>
+                <h3>Hair Color: {data.hair_color}</h3>
+                </StyledDetails>
+    )
+}
